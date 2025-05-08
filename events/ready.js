d8ffe21984d45803caa429c6c56df1fd5ba066e3
Domantas Palionis <97786702+DomantasDyz@users.noreply.github.com>
2025-05-08 12:29:10 +0300

    Added DB support for QR code scanner

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,13 @@
 const {Events, ActivityType} = require('discord.js');
 
+const Board  = require('../models/leaderbordoStructure'); //db
+
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     async execute(client, message) {
+        await Board.sync();
         console.log(`+ ${client.user.tag}`);
     }
 };
